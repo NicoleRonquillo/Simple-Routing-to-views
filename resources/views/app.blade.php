b507aa6660ceda97c72908bf0b08c128d55f5d08
NicoleRonquillo <144975101+NicoleRonquillo@users.noreply.github.com>
2025-03-12 10:06:05 +0000

    Tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html>
   <head>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
